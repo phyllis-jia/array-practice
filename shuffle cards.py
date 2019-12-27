dfb3f4660cb43d7ef112b7c120e1868db9495a7b
490232987 <54754413+490232987@users.noreply.github.com>
2019-12-27 00:13:21 -0500

    Create shuffle cards.py

---
+++ shuffle cards.py
@@ -0,0 +1,16 @@
+###shuffle cards
+#method 1
+import random
+def shuffle(cards):
+    random.shuffle(cards)
+    return cards
+
+#method 2
+def shuffle_list(cards):
+    for i in range(len(cards)):
+        randomi=i+random.randint(0,len(cards)-i-1)
+        cards[i], cards[randomi]=cards[randomi], cards[i]
+    return cards
+
+
+    
