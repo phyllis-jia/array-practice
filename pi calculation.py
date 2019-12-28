7a7d121ba83079f01f91e359667d652e94661d5d
490232987 <54754413+490232987@users.noreply.github.com>
2019-12-28 02:13:03 -0500

    Create pi calculation.py

---
+++ pi calculation.py
@@ -0,0 +1,27 @@
+###calculate pi
+## method 1: pi/4=1-1/3+1/5-....
+def pi_cal(n):
+    pi=0
+    a=1
+    for i in range(1, n+1, 2):
+        pi += 4*a*(1/i)
+        a *= -1
+    return pi
+
+### method 2
+def pi_cal2():
+    i=1
+    pi=0
+    a=1
+    pre=9999
+    delta=0.00001
+    
+    while (abs(pi-pre))>delta:
+        pre = pi
+        pi += a*(1/i)
+        a *= -1
+        i += 2
+    return pi*4
+        
+        
+    
