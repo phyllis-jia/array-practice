20b0482dd37cf94c8cae5066511d511c6210437c
490232987 <54754413+490232987@users.noreply.github.com>
2019-12-28 02:13:00 -0500

    Update singing contest.py

--- singing contest.py
+++ singing contest.py
@@ -16,6 +16,21 @@ def singing_score(values):
     rst=sum(values)/len(values)
     return rst
 
+###optimized two
+def singing_score2(values):
+    maxx=values[0]
+    minn=values[0]
+    summ=values[0]
+    for i in range(1,len(values)):
+        if values[i]<minn:
+            minn=values[i]
+        if values[i]>maxx:
+            maxx=values[i]
+        summ += values[i]
+        
+    rst=(summ-maxx-minn)/(len(values)-2)
+    return rst
+
 ### original one
 def score(a):
     h=max(a)
