299886c889ca2029c96c526b63c22aa531c82465
490232987 <54754413+490232987@users.noreply.github.com>
2019-12-31 01:50:57 -0500

    Update solution of quadratic eqution.py

--- solution of quadratic eqution.py
+++ solution of quadratic eqution.py
@@ -2,21 +2,21 @@
 ### method 1
 def result(a, b, c):
     if b**2-4*a*c > 0: 
-       x1=(-b+(b**2-4*a*c)**(1/2))/2*a
-       x2=(-b-(b**2-4*a*c)**(1/2))/2*a
+       x1 = (-b+(b**2-4*a*c)**(1/2))/2*a
+       x2 = (-b-(b**2-4*a*c)**(1/2))/2*a
        return(x1, x2)
     elif b**2-4*a*c == 0:
-        x=(-b+(b**2-4*a*c)**(1/2))/2*a
+        x = (-b+(b**2-4*a*c)**(1/2))/2*a
         return x
     else:
         print('error')
 ### method 2
 import math
 def solve(a, b, c):
-    v=pow(b,2)-4*a*c
+    v = pow(b,2)-4*a*c
     if v<0:
         raise ValueError('error')
-    return (-b+math.sqrt(v))/(2*a), (-b-math.sqrt(v))/(2*a)
+    return (-b + math.sqrt(v))/(2*a), (-b-math.sqrt(v))/(2*a)
 
 
     
