bd9392647b69ae5c6002c68c42eac97392b0912a
490232987 <54754413+490232987@users.noreply.github.com>
2019-12-30 23:12:50 -0500

    Create minesweeper.py

---
+++ minesweeper.py
@@ -0,0 +1,27 @@
+###minesweeper
+import random
+def minesweeper(m, n, p):
+    board=[[None]*(n+2) for i in range(m+2)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            r=random.random()
+            board[i][j]=-1 if r<p else 0
+
+    for i in range(1,m+1):
+        for j in range(1, n+1):
+            print('*', end=' ') if board[i][j]==-1 else print('.', end=' ')
+        print()
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if board[i][j]!=-1:
+                for ii in range(i-1, i+2):
+                    for jj in range(j-1,j+2):
+                        if board[ii][jj] == -1:
+                            board[i][j]+=1
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            print('*', end=' ') if board[i][j]==-1 else print(board[i][j], end=' ')
+        print()
+    
+   
