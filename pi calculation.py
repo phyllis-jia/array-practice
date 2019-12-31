c4945763fa676547b320ab09a6cabedadaf476fa
490232987 <54754413+490232987@users.noreply.github.com>
2019-12-31 01:50:48 -0500

    Update pi calculation.py

--- pi calculation.py
+++ pi calculation.py
@@ -1,8 +1,8 @@
 ###calculate pi
 ## method 1: pi/4=1-1/3+1/5-....
 def pi_cal(n):
-    pi=0
-    a=1
+    pi = 0
+    a = 1
     for i in range(1, n+1, 2):
         pi += 4*a*(1/i)
         a *= -1
@@ -10,13 +10,13 @@ def pi_cal(n):
 
 ### method 2
 def pi_cal2():
-    i=1
-    pi=0
-    a=1
-    pre=9999
-    delta=0.00001
+    i = 1
+    pi = 0
+    a = 1
+    pre = 9999
+    delta = 0.00001
     
-    while (abs(pi-pre))>delta:
+    while (abs(pi-pre)) > delta:
         pre = pi
         pi += a*(1/i)
         a *= -1
