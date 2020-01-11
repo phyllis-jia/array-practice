b7800cfc28400b33bfd2aceb751cb1d5a7926508
490232987 <54754413+490232987@users.noreply.github.com>
2020-01-11 02:04:49 -0500

    Update check conditional maximum.py

--- check conditional maximum.py
+++ check conditional maximum.py
@@ -1,15 +1,14 @@
 ###find the maximum number in given arrays, if it is more than two times of other numbers, return index. Or return -1
 def find_maximum(nums):
-    local = 0
+    maximum = second = 0
     for i in range(len(nums)):
-        if nums[i] >= local:
-            local = nums[i]
-    
-    for j in range(len(nums)):
-        if local >= 2*nums[j]:
-            return i
-        else:
-            return -1
+        if nums[i] > maximum:
+            second = maximum
+            maximum = nums[i]
+            idx = i
+        elif second < nums[i]:
+            second = nums[i]
+    return idx if maximum >= 2*second else -1
         
     
             
